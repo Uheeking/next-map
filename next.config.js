2955f5f54d75a6bb13ecb093d8a876decedba447
Uheeking <wert1847@naver.com>
2023-08-04 16:34:47 +0900

    ADD : 매장 정보 제목 및 설명 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['inflearn-nextjs.vercel.app', 'search.pstatic.net'],
+  },
+  i18n: {
+    locales: ['ko'],
+    defaultLocale: 'ko',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
