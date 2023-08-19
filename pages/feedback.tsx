9fdcb91df073664c81b90299a463ab7a50fa0199
Uheeking <wert1847@naver.com>
2023-08-19 20:10:31 +0900

    Revert "ADD : api 수정"
    This reverts commit 95940d9b0dfebf9b9e00e622ecae5a45875728f2.

--- pages/feedback.tsx
+++ pages/feedback.tsx
@@ -1,48 +1,20 @@
-import { Fragment } from 'react';
-import { GetServerSideProps, NextPage } from 'next';
-import { NextSeo } from 'next-seo';
-import Header from '../components/common/Header';
-import FeedbackSection from '../components/feedback/FeedbackSection';
-import type { Feedback } from '../types/feedback';
-import { getFeedbackListFromFirestore } from '../firebase/feedback';
+import { Fragment } from "react";
+import Header from "../components/common/Header";
+import { NextSeo } from "next-seo";
 
-interface Props {
-  initialFeedbackList: Feedback[];
-}
-
-export const FeedbackPage: NextPage<Props> = ({ initialFeedbackList }) => {
+export default function Feedback() {
   return (
     <Fragment>
       <NextSeo
         title="피드백"
         description="매장 지도 서비스에 대한 피드백을 받습니다."
-        canonical="https://inflearn-nextjs.vercel.app/feedback"
+        canonical="https://next-map-lemon.vercel.app/feedback"
         openGraph={{
-          url: 'https://inflearn-nextjs.vercel.app/feedback',
+          url: "https://next-map-lemon.vercel.app/feedback",
         }}
       />
       <Header />
-
-      <main
-        style={{
-          position: 'relative',
-          width: '100%',
-          height: '100%',
-          overflow: 'hidden',
-          touchAction: 'pinch-zoom',
-        }}
-      >
-        <FeedbackSection initialFeedbackList={initialFeedbackList} />
-      </main>
+      <main></main>
     </Fragment>
   );
-};
-export default FeedbackPage;
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    props: {
-      initialFeedbackList: await getFeedbackListFromFirestore(),
-    },
-  };
-};
+}
